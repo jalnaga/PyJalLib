f53d374b29b90d06381dfa90000e6a5d70cd9e7f
jalnaga <jalnagakds@gmail.com>
2025-04-26 21:46:08 +0900

    Import NamePart in addition to NamePartType for enhanced functionality

--- src/pyjallib/__init__.py
+++ src/pyjallib/__init__.py
@@ -9,7 +9,7 @@ Python library for game character development pipeline.
 __version__ = '1.0.0'
 
 # reload_modules 함수를 패키지 레벨에서 사용 가능하게 함
-from .namePart import NamePartType
+from .namePart import NamePart, NamePartType
 from .naming import Naming
 from .namingConfig import NamingConfig
 from .nameToPath import NameToPath
