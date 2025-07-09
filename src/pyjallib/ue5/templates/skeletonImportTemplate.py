95975f89d94fabed9af9f7ddb0efa92077ceea49
jalnaga <jalnagakds@gmail.com>
2025-07-09 10:40:16 +0900

    버전 번호를 0.1.17에서 0.1.18로 업데이트하고, NameToPath 클래스에 경로 정규화 및 폴더명 안전 변환 기능을 추가하여 코드의 안정성과 일관성을 향상시킴. 또한, gen_path 메서드에 실제 이름 포함 옵션을 추가하고, 관련 메서드의 별칭을 제공하여 호환성을 개선함.

--- src/pyjallib/ue5/templates/skeletonImportTemplate.py
+++ src/pyjallib/ue5/templates/skeletonImportTemplate.py
@@ -16,6 +16,6 @@ fbxPath = r'{inSkeletonFbxPath}'
 contentRootPrefix = r'{inContentRootPrefix}'
 fbxRootPrefix = r'{inFbxRootPrefix}'
 
-animImporter = SkeletonImporter(inContentRootPrefix=contentRootPrefix, inFbxRootPrefix=fbxRootPrefix)
+skeletonImporter = SkeletonImporter(inContentRootPrefix=contentRootPrefix, inFbxRootPrefix=fbxRootPrefix)
 
-result = animImporter.import_skeleton(fbxPath)
+result = skeletonImporter.import_skeleton(fbxPath)
